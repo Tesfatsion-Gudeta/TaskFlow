020b28a04bf4087449445cee5839afa1af317e23
Tesfatsion Gudeta <tesfatsiongudeta7@gmail.com>
2025-11-03 16:18:52 +0300

    add backend domain

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3001";
+const API_URL =
+  import.meta.env.VITE_API_URL ||
+  "https://task-manager-api-omega-two.vercel.app";
 
 export const api = axios.create({
   baseURL: API_URL,
